db19d8463319ebaececaa8f335429f66c1df9abf
balu83vs <balu83vs@gmail.com>
2024-02-22 11:30:45 +0300

    adaptation - add

--- not_assyncio_way/main.py
+++ not_assyncio_way/main.py
@@ -8,6 +8,8 @@ from abc import ABC, abstractmethod
 
 PRIORITY_QUEUE = []
 
+WARNING_TRAFFIC_LEVEL = 20
+
 
 ########################################  абстрактный класс светофора #################################################
 class UniversalTrafficLight(ABC):
@@ -66,11 +68,11 @@ class UniversalTrafficLight(ABC):
 
     # обработчик очереди сообщений
     def process_events(self):
-        status = f'обработал 1 сообщение'
+        status = f'обработал сообщения'
         crossroads_status(f'Светофор {self.id}', status)
         # пока очередь сообщений не пуста
         while self.event_queue:
-            event = self.event_queue.pop(0)                        # считывание первого сообщения 
+            event = self.event_queue.pop(0)    
             self.handle_event(event)         # передача сообщения в обработчик сообщений       
 
 
@@ -105,14 +107,15 @@ class UniversalTrafficLight(ABC):
 
 
     # установка приоритета (абстрактный метод)   
-    def grant_priority(self):
+    def grant_priority(self, status = None):
         self.set_priority(True)
-        status = f'получил приоритет'
         crossroads_status(f'Светофор {self.id}', status)
         self.red_to_green()
         self.set_queue_size(self.get_queue_size() - 1)
         status = f'проезд автомобиля по {self.get_state()} сигналу'
         crossroads_status(f'Светофор {self.id}', status)
+        if len(PRIORITY_QUEUE) > 0:
+            PRIORITY_QUEUE.pop(0)
 
 
     # сброс приоритета 
@@ -139,6 +142,23 @@ class UniversalTrafficLight(ABC):
     @property
     def controller(self):
 
+        adaptation_avto.check_warning_level(self) # проверка превышения опасного уровня траффика
+
+        # сбрасываем имеющийся приоритет   
+        # пока есть приоритет в очереди или превышение опасного уровня трафика, приоритет не снимается
+        if ((len(PRIORITY_QUEUE)>0 and PRIORITY_QUEUE[0] == self.id) or 
+               (self.queue_size > WARNING_TRAFFIC_LEVEL and 
+                self.queue_size == max([light.queue_size for light in avto_lights]))):  
+            if self.get_priority():
+                self.set_queue_size(self.get_queue_size() - 1)
+                status = f'проезд автомобиля по {self.get_state()} сигналу'
+                crossroads_status(f'Светофор {self.id}', status)
+                if len(PRIORITY_QUEUE) > 0:
+                    PRIORITY_QUEUE.pop(0)
+        else:    
+            if self.get_priority():
+                self.drop_priority() 
+
         # начальный трафик с камеры контроля
         if self.get_queue_size() < 0:
             self.set_queue_size(self.camera.get_queue_size())
@@ -146,30 +166,26 @@ class UniversalTrafficLight(ABC):
             crossroads_status(f'Светофор {self.id}', status)
         # обновление данных трафика с камеры контроля    
         else:
-            self.set_queue_size(self.get_queue_size() + random.randrange(0,5)) 
+            self.set_queue_size(self.get_queue_size() + random.randrange(0,10)) 
             status = f'получил прибавку трафика от камеры'
             crossroads_status(f'Светофор {self.id}', status)
 
         # проверка приоритета
         if len(PRIORITY_QUEUE)>0:
-            if self.id == PRIORITY_QUEUE[0]:
-                self.grant_priority()   
+            if self.id == PRIORITY_QUEUE[0] and not self.get_priority():
+                status = f'получил приоритет'
+                self.grant_priority(status)   
                 
         # отправка запроса приоритета другим светофорам (пока приоритет не получен)
         if not self.get_priority():
-            for traffic_light in auto_lights:
+            for traffic_light in avto_lights:
                 if self.id != traffic_light.id:
                     self.request_priority(traffic_light) 
 
-        # отправляем одно сообщение из очереди в обработчик            
-        self.process_events()
+        # отправляем сообщения из очереди в обработчик            
+        self.process_events()       
 
-        # сбрасываем имеющийся приоритет    
-        if self.get_priority():
-            self.drop_priority()
-
-            if len(PRIORITY_QUEUE) > 0:
-                PRIORITY_QUEUE.pop(0)    
+                
 #####################################  конец класса абстрактного светофора ###########################################
 
 
@@ -237,11 +253,43 @@ class Camera:
 
 
 
+############################################### класс адаптивного модуля ################################################
+class Adaptation:
+    def __init__(self, lights):
+        self.lights = lights
+        self.slave_lights = {}
+
+    def get_slave_lights(self):
+        return self.slave_lights    
+
+    def set_slave_lights(self, slave_lights):
+        self.set_slave_lights = slave_lights      
+
+    # формируем список зависимых светофоров
+    def create_slave(self, current_light):
+        if current_light.id%2 == 0:
+            self.slave_lights.setdefault(current_light.id, 
+                                         [light.id for light in self.lights 
+                                          if light.id%2 == 0 and light.id != current_light.id])
+        else:
+            self.slave_lights.setdefault(current_light.id, 
+                                         [light.id for light in self.lights 
+                                          if light.id%2 != 0 and light.id != current_light.id])
+
+    def check_warning_level(self, other):
+        if other.queue_size > WARNING_TRAFFIC_LEVEL and other.queue_size == max([light.queue_size for light in avto_lights]):
+            if not other.get_priority():
+                other.grant_priority('получил приоритет по максимальному трафику') 
+
+############################################### конец адаптивного модуля ###############################################
+
+
+
 ############################################# Блок управляющих функций #################################################
 # подключение светофоров к перекрестку
 def create_lights():
     # экземпляры автомобильных светофоров
-    auto_lights = [
+    avto_lights = [
         AutoTrafficLight(1, Camera(), False),
         AutoTrafficLight(2, Camera(), False),
         AutoTrafficLight(3, Camera(), False),
@@ -259,7 +307,17 @@ def create_lights():
         PeopleTrafficLight(11, Camera(), False),
         PeopleTrafficLight(12, Camera(), False)
         ]
-    return auto_lights, people_lights
+    return avto_lights, people_lights
+
+
+# подключение адаптаций светофоров
+def connect_adaptation():
+
+    # экземпляры адаптационного модуля
+    adaptation_avto = Adaptation(avto_lights)           # подключение модуля адаптации для авто
+    adaptation_people = Adaptation(people_lights)       # подключение модуля адаптации для пешеходов
+    
+    return adaptation_avto, adaptation_people
 
 
 # опрос светофоров перекрестка
@@ -271,9 +329,9 @@ def crossroads_status(current_id = 'Обновлений', status = 'НЕТ'):
 
     # вывод информации об автомобильных светофорах
     print('*'*15, 'Auto traffic light:', '*'*15)
-    for auto_light in auto_lights:
-        current_queue_size = auto_light.get_queue_size()
-        print(auto_light.id, auto_light.get_state(), auto_light.get_priority(), current_queue_size) 
+    for avto_light in avto_lights:
+        current_queue_size = avto_light.get_queue_size()
+        print(avto_light.id, avto_light.get_state(), avto_light.get_priority(), current_queue_size) 
     
     # вывод информации о пешеходных светофорах
     #print('*'*14, 'People traffic light:', '*'*14)    
@@ -284,16 +342,24 @@ def crossroads_status(current_id = 'Обновлений', status = 'НЕТ'):
 
 
 # главная компилирующая функция перекрестка   
-def main(auto_lights, people_lights):     
+def main():     
     while True:
-        for autolight in auto_lights:
-            autolight.controller  
+        for avtolight in avto_lights:
+            # если список зависимостей светофора пуст
+            if not adaptation_avto.slave_lights.get(avtolight.id):
+                adaptation_avto.create_slave(avtolight)         # подготовка зависимостей авто светофоров
+            avtolight.controller  
 ############################################# конец блока управляющих функций ##########################################
 
 
 # точка входа
 if __name__ == '__main__':
-    auto_lights, people_lights = create_lights()        # набор автомобильных и пешеходных светофоров
-    traffic_lights = auto_lights.copy()
-    traffic_lights.extend(people_lights)
-    main(auto_lights, people_lights)                    # запуск цикла событий
+    avto_lights, people_lights = create_lights()                # набор автомобильных и пешеходных светофоров
+
+    adaptation_avto, adaptation_people = connect_adaptation()   # экземпляры класса адаптации    
+
+    traffic_lights = avto_lights.copy()                     
+    traffic_lights.extend(people_lights)                        # сообщество всех светофоров перекрестка
+
+    main()                                                      # запуск цикла событий
+    
